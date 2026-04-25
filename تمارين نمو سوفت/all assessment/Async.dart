e6026bc9ca3e49d065a899bb9d618ea5729edaa0
altmimialibnali-oss <altmimialibnali@gmail.com>
2026-04-26 00:32:49 +0300

    Delete تمارين نمو سوفت/all assessment directory

--- تمارين نمو سوفت/all assessment/Async.dart
+++
@@ -1,4 +0,0 @@
-Future<String> fetchUserData() async {
-  await Future.delayed(Duration(seconds: 3));
-  return "User Data";
-}
--- تمارين نمو سوفت/all assessment/assigment10.dart
+++
@@ -1,8 +0,0 @@
-String? name = null;
-// String name2 = null; ❌ خطأ
-List<int>? a;
-List<int> b = [1, 2, ...?a];
-String? username;
-print(username ?? "Guest");
-print(username?.length);
-String value = username!;
